d660f8f7a2dd629ccd5d1a18aa309f94897d26d9
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-09-12 15:24:16 +0900

    Fix TrackingId

--- app/mvc/util/TrackingId.scala
+++ app/mvc/util/TrackingId.scala
@@ -3,12 +3,5 @@ package mvc.util
 import java.util.UUID
 
 trait TrackingId {
-  val generateUUID = UUID.randomUUID
-
-  /**
-   * Give the Log a tracking ID
-   */
-  def GrantTrackingId(message: String): String = {
-    s"[trackingId=${generateUUID}] ${message}"
-  }
+  lazy val generateUUID = UUID.randomUUID
 }
