2e8b5ec71859aa8713e63ad020c4f1687a5ceaee
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-09-12 15:24:31 +0900

    Fix TrackingLogger

--- app/mvc/util/TrackingLogger.scala
+++ app/mvc/util/TrackingLogger.scala
@@ -1,39 +1,45 @@
 package mvc.util
 
-import scala.reflect.ClassTag
-
-import org.slf4j.Logger
+import org.slf4j.Marker
+import org.slf4j.MarkerFactory
 import org.slf4j.LoggerFactory
+import org.slf4j.spi.LocationAwareLogger
 
 trait TrackingLogging {
-  lazy val trackingLogger: TrackingLogger = {
-    TrackingLogger(LoggerFactory.getLogger(getClass))
+  val trackingLogger: TrackingLogger = {
+    TrackingLogger(LoggerFactory.getLogger(getClass).asInstanceOf[LocationAwareLogger])
   }
 }
 
-sealed case class TrackingLogger(logger: Logger) extends TrackingId {
+import LocationAwareLogger._
+protected case class TrackingLogger(logger: LocationAwareLogger) extends TrackingId {
+
+  val marker: Marker = MarkerFactory.getMarker(s"[trackingId=${generateUUID}]")
 
   /**
    * Message only log
    */
-  def info(message:  => String): Unit = { if (logger.isInfoEnabled)  logger.info(GrantTrackingId(message))  }
-  def warn(message:  => String): Unit = { if (logger.isWarnEnabled)  logger.warn(GrantTrackingId(message))  }
-  def error(message: => String): Unit = { if (logger.isErrorEnabled) logger.error(GrantTrackingId(message)) }
-  def debug(message: => String): Unit = { if (logger.isDebugEnabled) logger.debug(GrantTrackingId(message)) }
-  def trace(message: => String): Unit = { if (logger.isTraceEnabled) logger.trace(GrantTrackingId(message)) }
+  def info(message:  => String): Unit = { if (logger.isInfoEnabled)  logger.log(marker, getClass.getName, INFO_INT,  message, null, null) }
+  def warn(message:  => String): Unit = { if (logger.isWarnEnabled)  logger.log(marker, getClass.getName, WARN_INT,  message, null, null) }
+  def error(message: => String): Unit = { if (logger.isErrorEnabled) logger.log(marker, getClass.getName, ERROR_INT, message, null, null) }
+  def debug(message: => String): Unit = { if (logger.isDebugEnabled) logger.log(marker, getClass.getName, DEBUG_INT, message, null, null) }
+  def trace(message: => String): Unit = { if (logger.isTraceEnabled) logger.log(marker, getClass.getName, TRACE_INT, message, null, null) }
 
   /**
    * Handle messages and exceptions
    */
-  def info(message:  => String, throwable: Throwable): Unit = { if (logger.isInfoEnabled)  logger.info(GrantTrackingId(message),  throwable) }
-  def warn(message:  => String, throwable: Throwable): Unit = { if (logger.isWarnEnabled)  logger.warn(GrantTrackingId(message),  throwable) }
-  def error(message: => String, throwable: Throwable): Unit = { if (logger.isErrorEnabled) logger.error(GrantTrackingId(message), throwable) }
-  def debug(message: => String, throwable: Throwable): Unit = { if (logger.isDebugEnabled) logger.debug(GrantTrackingId(message), throwable) }
-  def trace(message: => String, throwable: Throwable): Unit = { if (logger.isTraceEnabled) logger.trace(GrantTrackingId(message), throwable) }
-}
+  def info(message:  => String, throwable: Throwable): Unit = { if (logger.isInfoEnabled)  logger.log(marker, getClass.getName, INFO_INT,  message, null, throwable) }
+  def warn(message:  => String, throwable: Throwable): Unit = { if (logger.isWarnEnabled)  logger.log(marker, getClass.getName, WARN_INT,  message, null, throwable) }
+  def error(message: => String, throwable: Throwable): Unit = { if (logger.isErrorEnabled) logger.log(marker, getClass.getName, ERROR_INT, message, null, throwable) }
+  def debug(message: => String, throwable: Throwable): Unit = { if (logger.isDebugEnabled) logger.log(marker, getClass.getName, DEBUG_INT, message, null, throwable) }
+  def trace(message: => String, throwable: Throwable): Unit = { if (logger.isTraceEnabled) logger.log(marker, getClass.getName, TRACE_INT, message, null, throwable) }
 
-object TrackingLogger {
-  def apply[T](implicit classTag: ClassTag[T]): TrackingLogger = {
-    TrackingLogger(LoggerFactory.getLogger(classTag.runtimeClass))
-  }
+  /**
+   * Handle messages and Array Object
+   */
+  def info(message:  => String, argArray: Array[Object]): Unit = { if (logger.isInfoEnabled)  logger.log(marker, getClass.getName, INFO_INT,  message, argArray, null) }
+  def warn(message:  => String, argArray: Array[Object]): Unit = { if (logger.isWarnEnabled)  logger.log(marker, getClass.getName, WARN_INT,  message, argArray, null) }
+  def error(message: => String, argArray: Array[Object]): Unit = { if (logger.isErrorEnabled) logger.log(marker, getClass.getName, ERROR_INT, message, argArray, null) }
+  def debug(message: => String, argArray: Array[Object]): Unit = { if (logger.isDebugEnabled) logger.log(marker, getClass.getName, DEBUG_INT, message, argArray, null) }
+  def trace(message: => String, argArray: Array[Object]): Unit = { if (logger.isTraceEnabled) logger.log(marker, getClass.getName, TRACE_INT, message, argArray, null) }
 }
