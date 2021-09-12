23eac721d5a679635523f1f4344b2d66cb787a0d
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-09-12 14:04:17 +0900

    Fix TrackingLogger

--- app/mvc/util/TrackingLogger.scala
+++ app/mvc/util/TrackingLogger.scala
@@ -1,9 +1,9 @@
 package mvc.util
 
+import scala.reflect.ClassTag
+
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-
-import scala.reflect.ClassTag
 
 trait TrackingLogging {
   lazy val trackingLogger: TrackingLogger = {
@@ -16,8 +16,8 @@ sealed case class TrackingLogger(logger: Logger) extends TrackingId {
   /**
    * Message only log
    */
-  def info(message: => String):  Unit = { if (logger.isInfoEnabled)  logger.info(GrantTrackingId(message))  }
-  def warn(message: => String):  Unit = { if (logger.isWarnEnabled)  logger.warn(GrantTrackingId(message))  }
+  def info(message:  => String): Unit = { if (logger.isInfoEnabled)  logger.info(GrantTrackingId(message))  }
+  def warn(message:  => String): Unit = { if (logger.isWarnEnabled)  logger.warn(GrantTrackingId(message))  }
   def error(message: => String): Unit = { if (logger.isErrorEnabled) logger.error(GrantTrackingId(message)) }
   def debug(message: => String): Unit = { if (logger.isDebugEnabled) logger.debug(GrantTrackingId(message)) }
   def trace(message: => String): Unit = { if (logger.isTraceEnabled) logger.trace(GrantTrackingId(message)) }
@@ -25,8 +25,8 @@ sealed case class TrackingLogger(logger: Logger) extends TrackingId {
   /**
    * Handle messages and exceptions
    */
-  def info(message: => String, throwable: Throwable):  Unit = { if (logger.isInfoEnabled)  logger.info(GrantTrackingId(message), throwable)  }
-  def warn(message: => String, throwable: Throwable):  Unit = { if (logger.isWarnEnabled)  logger.warn(GrantTrackingId(message), throwable)  }
+  def info(message:  => String, throwable: Throwable): Unit = { if (logger.isInfoEnabled)  logger.info(GrantTrackingId(message),  throwable) }
+  def warn(message:  => String, throwable: Throwable): Unit = { if (logger.isWarnEnabled)  logger.warn(GrantTrackingId(message),  throwable) }
   def error(message: => String, throwable: Throwable): Unit = { if (logger.isErrorEnabled) logger.error(GrantTrackingId(message), throwable) }
   def debug(message: => String, throwable: Throwable): Unit = { if (logger.isDebugEnabled) logger.debug(GrantTrackingId(message), throwable) }
   def trace(message: => String, throwable: Throwable): Unit = { if (logger.isTraceEnabled) logger.trace(GrantTrackingId(message), throwable) }
